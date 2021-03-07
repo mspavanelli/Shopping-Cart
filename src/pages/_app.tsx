f6b8bba7f425181be2de0686cdaf735c355a75de
Matheus Pavanelli <mspavanelli@gmail.com>
2021-03-07 19:57:30 -0300

    :package: chore: add and setup miragejs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,14 @@ import { ChakraProvider } from '@chakra-ui/react'
 
 import theme from 'theme'
 
+import { makeServer } from '../miragejs/server'
+
+console.log(process.env.NODE_ENV)
+
+if (process.env.NODE_ENV === 'development') {
+  makeServer({ environment: 'development' })
+}
+
 function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider resetCSS theme={theme}>
